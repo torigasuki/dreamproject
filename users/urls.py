441676055017952825e6417d160d1fef41bf6626
김태연 <kimtaeyeon@gimtaeyeon-ui-MacBookAir.local>
2023-05-09 20:07:12 +0900

    signup login update

---
+++ users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from users import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('signup/', views.UserView.as_view(), name='user_view'),
+    path('mock/', views.mockView.as_view(), name='mock_view'),
+    path('login/', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),    
+]
