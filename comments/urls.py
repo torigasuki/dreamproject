c8b9a8cf00d238d6d3f529cff03b9cd515451188
Jeong SeungHo <teapackage16@gmail.com>
2023-05-09 11:45:32 +0900

    url수정

--- comments/urls.py
+++ comments/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from comments import views
 
 urlpatterns = [
-    path('', views.Comment.as_view(), name='comment'),
-    path('<int:comment_id>/', views.Comment.as_view(), name='comment_ud'),
-    path('<int:comment_id>/re/', views.ReComment.as_view(), name='re_comment'),
-    path('<int:comment_id>/re/<int:recomment_id>', views.ReComment.as_view(), name='re_comment_ud')
+    path('', views.CommentView.as_view(), name='comment'),
+    path('<int:comment_id>/', views.CommentView.as_view(), name='comment_ud'),
+    path('<int:comment_id>/re/', views.ReCommentView.as_view(), name='re_comment'),
+    path('<int:comment_id>/re/<int:recomment_id>', views.ReCommentView.as_view(), name='re_comment_ud')
 ]
