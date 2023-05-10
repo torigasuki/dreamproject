ae52ad1fe604729eba5421fbf7ffe453322f5075
Jeong SeungHo <teapackage16@gmail.com>
2023-05-10 17:54:46 +0900

    이메일인증기능구현

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from users.models import User
-
+from users.models import User, Verify
+from django.shortcuts import get_object_or_404
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta :
@@ -30,3 +30,19 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
         token = super().get_token(user)
         token['email'] = user.email
         return token
+    
+    
+class VerificationCodeSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    code = serializers.CharField(max_length=6)
+    
+    def validate(self, data):
+        # attrs에서 email과 code 값 추출
+        email = data.get('email')
+        code = data.get('code')
+        verify = get_object_or_404(Verify, email=email)
+        if code == verify.code:  # 인증코드 일치 여부 확인
+            verify.delete()  # 인증코드 삭제
+            return data
+        else:
+            raise serializers.ValidationError("Invalid verification code")    
